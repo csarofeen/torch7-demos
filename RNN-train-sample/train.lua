0f84b81504b3fd6ebdd0ee473fd0b0713c9c5e95
Eugenio <culurciello@gmail.com>
2016-09-09 12:32:04 -0400

    added model utils and test function

--- RNN-train-sample/train.lua
+++ RNN-train-sample/train.lua
@@ -1,63 +1,77 @@
 -- Eugenio Culurciello
 -- September 2016
 -- RNN training test: ABBA sequence detector
+-- based on: https://raw.githubusercontent.com/karpathy/char-rnn/master/model/RNN.lua
 
 require 'nn'
 require 'nngraph'
 require 'optim'
-require 'RNN' -- https://raw.githubusercontent.com/karpathy/char-rnn/master/model/RNN.lua
+require 'RNN'
 -- local c = require 'trepl.colorize'
+local model_utils = require 'model_utils'
 
 torch.setdefaulttensortype('torch.FloatTensor')
 -- nngraph.setDebug(true)
 
-local dictionary_size = 2 -- sequence of 2 symbols
-local seq_length = 1000 -- sequence length
-local nT = 4 -- RNN time steps
+local opt = {}
+opt.dictionary_size = 2 -- sequence of 2 symbols
+opt.train_size = 1000 -- train data size
+opt.seq_length = 4 -- RNN time steps
 
 print('Creating Input...')
 -- create a sequence of 2 numbers: {2, 1, 2, 2, 1, 1, 2, 2, 1 ..}
-local s = torch.ceil(torch.rand(seq_length):add(0.5))
+local s = torch.ceil(torch.rand(opt.train_size):add(0.5))
 -- print('Inputs sequence:', s)
-local y = torch.ones(1,seq_length)
-for i = 4, seq_length do -- if you find sequence ...1001... then output is class '2', otherwise is '1'
+local y = torch.ones(1,opt.train_size)
+for i = 4, opt.train_size do -- if you find sequence ...1001... then output is class '2', otherwise is '1'
    if (s[{i-3}]==2 and s[{i-2}]==1 and s[{i-1}]==1 and s[{i}]==2) then y[{1,{i}}] = 2 end
 end
 -- print('Desired output sequence:', y)
-local x = torch.zeros(2,seq_length) -- create input with 1-hot encoding:
-for i = 1, seq_length do
+local x = torch.zeros(2, opt.train_size) -- create input with 1-hot encoding:
+for i = 1, opt.train_size do
    if y[{1,{i}}] == 2 then x[{{},{i}}] = torch.ones(2) end
 end
 -- print('Input vector:', x)
 
 -- model:
 print('Creating Model...')
-local rnn_size = 1
-local rnn_layers = 1
-local batch_size = 1
-local RNNmodel = RNN(dictionary_size, rnn_size, rnn_layers, 0) -- input = 2 (classes), 1 layer, rnn_size=1, no dropout 
--- print('Test of RNN output:', RNNmodel:forward{ torch.Tensor(2), torch.Tensor(1) })
+opt.rnn_size = 1
+opt.rnn_layers = 1
+opt.batch_size = 1
 
-local params, grad_params 
-params, grad_params = RNNmodel:getParameters()
-print('Number of parameters in the model: ' .. params:nElement())
--- print(params, grad_params)
+local protos = {} 
+protos.rnn = RNN(opt.dictionary_size, opt.rnn_size, opt.rnn_layers, 0) -- input = 2 (classes), 1 layer, rnn_size=1, no dropout 
+protos.criterion = nn.ClassNLLCriterion()
+-- print('Test of RNN output:', RNNmodel:forward{ torch.Tensor(2), torch.Tensor(1) })
 
 -- the initial state of the cell/hidden states
 local init_state = {}
-for L=1,rnn_layers do
-  local h_init = torch.zeros(batch_size, rnn_size)
+for L = 1, opt.rnn_layers do
+  local h_init = torch.zeros(opt.batch_size, opt.rnn_size)
   table.insert(init_state, h_init:clone())
 end
 
+local params, grad_params 
+-- get flattened parameters tensor
+params, grad_params = model_utils.combine_all_parameters(protos.rnn)
+-- params, grad_params = protos.rnn:getParameters()
+print('Number of parameters in the model: ' .. params:nElement())
+-- print(params, grad_params)
+
 -- create clones of model to unroll in time:
+print('Cloning RNN model:')
 local clones = {}
 clones.rnn = {}
 clones.criterion = {}
-for i=1,nT do
-   clones.rnn[i] = RNNmodel:clone('weight', 'gradWeights', 'bias')
-   clones.criterion[i] = nn.ClassNLLCriterion()
+for i = 1,opt.seq_length do
+   clones.rnn[i] = protos.rnn:clone('weight', 'gradWeights', 'bias')
+   clones.criterion[i] = protos.criterion:clone()
 end
+-- for name, proto in pairs(protos) do
+--   print('cloning ' .. name)
+--   clones.rnn = model_utils.clone_many_times(proto, opt.seq_length, not proto.parameters)
+-- end
+-- print(clones)
 
 function clone_list(tensor_list, zero_too)
   -- takes a list of tensors and returns a list of cloned tensors
@@ -69,23 +83,23 @@ function clone_list(tensor_list, zero_too)
   return out
 end
 
-
 -- training function:
 local init_state_global = clone_list(init_state)
 local bo = 0 -- batch counter
+opt.grad_clip = 5
 function feval(p)
   if p ~= params then
     params:copy(p)
   end
   grad_params:zero()
 
-  local predictions = {}
-  local rnn_state = {[0]=init_state_global} -- initial state
-  local loss = 0
-
-  -- forward pass ---------------------------------------------------------------
   -- bo variable creates batches on the fly
-  for t = 1, nT do
+  
+  -- forward pass ---------------------------------------------------------------
+  local rnn_state = {[0]=init_state_global} -- initial state
+  local predictions = {}
+  local loss = 0
+  for t = 1, opt.seq_length do
     clones.rnn[t]:training() -- make sure we are in correct training mode
     local  lst = clones.rnn[t]:forward{x[{{},{t+bo}}]:t(), unpack(rnn_state[t-1])}
     rnn_state[t] = {}
@@ -93,12 +107,11 @@ function feval(p)
     predictions[t] = lst[#lst]
     loss = loss + clones.criterion[t]:forward(predictions[t], y[{1,{t+bo}}])
   end
-  loss = loss / nT 
-
+  loss = loss / opt.seq_length
   -- backward pass --------------------------------------------------------------
   -- initialize gradient at time t to be zeros (there's no influence from future)
-  local drnn_state = {[nT] = clone_list(init_state, true)} -- true also zeros the clones
-  for t=nT,1,-1 do
+  local drnn_state = {[opt.seq_length] = clone_list(init_state, true)} -- true also zeros the clones
+  for t = opt.seq_length, 1, -1 do
     -- print(drnn_state)
     -- backprop through loss, and softmax/linear
     -- print(predictions[t], y[{1,{t+bo}}])
@@ -117,31 +130,60 @@ function feval(p)
   end
   -- transfer final state to initial state (BPTT)
   init_state_global = rnn_state[#rnn_state]
-
-  -- create next batch:
-  bo = bo + 1
-
-  grad_params:div(nT)
+  -- grad_params:div(opt.seq_length)
   -- clip gradient element-wise
-  grad_params:clamp(-5, 5)
-
+  grad_params:clamp(-opt.grad_clip, opt.grad_clip)
   -- print(params,grad_params)
-
+  -- point to next batch:
+  bo = bo + 1
   return loss, grad_params
 end
 
 
 -- training:
+opt.learning_rate = 2e-3
+opt.decay_rate = 0.95
 print('Training...')
 local losses = {}
-local optim_state = {learningRate = 1e-1}
-local iterations = seq_length-nT
+local optim_state = {learningRate = opt.learning_rate, alpha = opt.decay_rate}
+local iterations = opt.train_size - opt.seq_length + 1
 for i = 1, iterations do
     local _, loss = optim.rmsprop(feval, params, optim_state)
     losses[#losses + 1] = loss[1]
 
-    if i % (seq_length/10) == 0 then
-        print(string.format("Iteration %8d, loss = %4.4f, loss/seq_len = %4.4f, gradnorm = %4.4e", i, loss[1], loss[1] / nT, grad_params:norm()))
+    if i % (opt.train_size/10) == 0 then
+        print(string.format("Iteration %8d, loss = %4.4f, loss/seq_len = %4.4f, gradnorm = %4.4e", i, loss[1], loss[1] / opt.seq_length, grad_params:norm()))
     end
 end
 
+
+-- testing function:
+bo = 0
+function test()
+  -- bo variable creates batches on the fly
+  
+  -- forward pass ---------------------------------------------------------------
+  local rnn_state = {[0]=init_state_global} -- initial state
+  local predictions = {}
+  local loss = 0
+  for t = 1, opt.seq_length do
+    clones.rnn[t]:evaluate() -- make sure we are in correct testing mode
+    local  lst = clones.rnn[t]:forward{x[{{},{t+bo}}]:t(), unpack(rnn_state[t-1])}
+    rnn_state[t] = {}
+    for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end -- extract the state, without output
+    predictions[t] = lst[#lst]
+    loss = loss + clones.criterion[t]:forward(predictions[t], y[{1,{t+bo}}])
+  end
+  -- carry over lstm state
+  rnn_state[0] = rnn_state[#rnn_state]
+  -- print results:
+  print('Prediction:', predictions[opt.seq_length][{{1},{1}}][1][1], 'Label:', y[{1,{opt.seq_length+bo}}][1])
+  -- point to next batch:
+  bo = bo + 1
+end
+
+-- and test!
+opt.test_samples = 10
+for i = 1, opt.test_samples do
+  test()
+end
